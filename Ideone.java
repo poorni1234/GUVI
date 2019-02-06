66c5900e3104f4727c6f75c4372f4ed02411a298
poorni1234 <47376824+poorni1234@users.noreply.github.com>
2019-02-06 00:50:34 -0800

    Create Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        number = scan.nextInt();
+        scan.close();
+        if(number > 0)
+        {
+            System.out.println(number+"positive");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" zero");
+        }
+}
+}
